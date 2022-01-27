b9bfe2a2bcd1852d4af317aecdc3aa61bbc8b810
Giorgi Gordiashvili <giorgi.gordiashvili.1@btu.edu.ge>
2022-01-27 23:11:59 +0400

    Jimsher page (#3)
    Co-authored-by: Gordela <giorgigordiashvili@Giorgis-MacBook-Pro.local>

--- js/index.js
+++ js/index.js
@@ -4,12 +4,13 @@ $(function () {
       $("#navbar").slideDown("slow");
       $("#navbar").css("display", "flex");
       $("#nav").text("CLOSE");
-
+      $("#nav-secondary").css("z-index", "4");
       $("body").addClass("fixed-position");
     } else {
       $("#navbar").slideUp();
       $("#nav").text("MENU");
       $("body").removeClass("fixed-position");
+      $("#nav-secondary").css("z-index", "6");
     }
   });
 });
